4a2ff2512b69821959cead98c034d46f6142411e
wwkd623 <wwkd623@naver.com>
2025-10-18 03:29:56 +0900

    Add files via upload

--- PC/snuh/js/main.js
+++ PC/snuh/js/main.js
@@ -1,10 +1,64 @@
 $(function(){
+  $(document).click(function(e){
+    e.preventDefault();
+  });
+
   $(window).on("scroll", function () {
     if ($(this).scrollTop() > 60) {
       $(".logo_wrap").slideUp(100);
     } else {
       $(".logo_wrap").slideDown(100);
     }
+  });
+
+  let $prevDepth = null;
+
+  $("ul.gnb>li").mouseenter(
+    function () {
+      $("ul.gnb>li").find(".depth2").removeClass("on");
+      $(this).find(".depth2").addClass("on");
+    },
+    function () {
+      const $currentDepth = $(this).find(".depth2");
+
+      if ($prevDepth && $prevDepth[0] !== $currentDepth[0]) {
+        $prevDepth.css("transition", "none").removeClass("on");
+        $currentDepth.css("transition", "none").addClass("on");
+
+        setTimeout(() => {
+          $(".depth2").css("transition", "height 0.3s ease-in-out");
+        }, 10);
+      } else {
+        $currentDepth.addClass("on");
+      }
+
+      $prevDepth = $currentDepth;
+    },
+    function () {
+      const $currentDepth = $(this).find(".depth2");
+      $currentDepth.removeClass("on");
+      $prevDepth = null;
+    }
+  );
+
+  $(function () {
+    const $gnb = $(".gnb");
+    $gnb.append('<span class="gnb_line"></span>');
+    const $line = $gnb.find(".gnb_line");
+
+    $(".gnb>li").on("mouseenter", function () {
+      const left = $(this).position().left;
+      const width = $(this).outerWidth();
+      $line.css({ left, width, opacity: 1 });
+    });
+
+    $("header").on("mouseleave", function () {
+      $line.css({ opacity: 0 });
+    });
+  });
+
+  $("header").mouseleave(function(){
+    $("ul.gnb>li").find(".depth2").removeClass("on");
   });
 
   var mainSwiper = new Swiper(".slider", {
@@ -14,7 +68,7 @@ $(function(){
       delay: 3000,
       disableOnInteraction: false,
     },
-    speed: 2000,
+    speed: 1800,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -66,5 +120,16 @@ $(function(){
     targetSwiper.autoplay.start();
     $(this).css({ opacity: 0, pointerEvents: "none" });
     $(this).siblings(".btn_pause").css({ opacity: 1, pointerEvents: "auto" });
+  });
+
+  $("ul.family>li").click(function(){
+    $("ul.family>li").hide();
+    $("button.close").show();
+    $(".family_list").addClass("on");
+  });
+  $("button.close").click(function(){
+    $("button.close").hide();
+    $("ul.family>li").show();
+    $(".family_list").removeClass("on");
   });
 });
